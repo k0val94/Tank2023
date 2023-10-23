322a1cc5eccd0b0fe2d1755083dafd0499ef9c47
k0val94 <k0val94@users.noreply.github.com>
2023-10-23 18:48:57 +0200

    tidy up

--- Assets/Scripts/MapCreatorController.cs
+++ Assets/Scripts/MapCreatorController.cs
@@ -60,48 +60,15 @@ public class MapCreatorController : MonoBehaviour
         Debug.Log("Attempting to generate map.");
         int mapWidth, mapHeight;
         List<string[]> loadedMap = null;
-
-        if (mapCleaner != null)
-        {
-            mapCleaner.CleanupMap();
-        }
+        mapCleaner.CleanupMap();
 
         if (int.TryParse(widthInputField.text, out mapWidth) && int.TryParse(heightInputField.text, out mapHeight))
         {
             Debug.Log($"Generating map with dimensions: {mapWidth}x{mapHeight}");
-
-
             mapGenerator.GenerateRandomMap(mapWidth, mapHeight);
-            
-            if (mapSaver != null)
-            {
-                mapSaver.SaveMapToFile(mapGenerator.GetMapLayers(), "temp.map");
-            }
-            
-
-            if (mapLoader != null)
-            {
-                Debug.Log("Attempting to load map from file.");
-                loadedMap = mapLoader.LoadMapFromFile("temp.map");
-                if (loadedMap == null || loadedMap.Count == 0)
-                {
-                    Debug.LogError("No data loaded from map file!");
-                }
-            }
-            else
-            {
-                Debug.LogError("Map Loader not assigned or not found!");
-            }
-
-            if (mapBuilder != null && loadedMap != null)
-            {
-                Debug.Log("Attempting to build map from loaded data.");
-                mapBuilder.BuildMap(loadedMap);
-            }
-            else
-            {
-                Debug.LogError("Map Builder not assigned or not found or map data not loaded properly!");
-            }
+            mapSaver.SaveMapToFile(mapGenerator.GetMapLayers(), "temp.map");
+            loadedMap = mapLoader.LoadMapFromFile("temp.map");
+            mapBuilder.BuildMap(loadedMap);
         }
         else
         {
