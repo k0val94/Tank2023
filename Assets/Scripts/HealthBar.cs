7f8bbb9ae94671259e503858513f611a85399864
k0val94 <k0val94@users.noreply.github.com>
2023-11-19 17:56:43 +0100

    healthbar now green to red

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,34 +6,54 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     private Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
- 	void Start()
+    void Awake()
     {
-
         slider = GetComponent<Slider>();
         if (slider == null)
         {
-            Debug.LogError("HealthBar script is not attached to an object with a Slider component.");
+            Debug.LogError("Slider component not found on HealthBar GameObject in Awake().");
+            return; 
         }
 
-		RectTransform rectTransform = GetComponent<RectTransform>();
-		rectTransform.anchorMin = new Vector2(0, 0);
-		rectTransform.anchorMax = new Vector2(0, 0);
-		rectTransform.pivot = new Vector2(0, 0);
-		rectTransform.anchoredPosition = new Vector2(10, 10); // Anpassen, wie benötigt
-		rectTransform.sizeDelta = new Vector2(200, 30); // Setzen Sie die gewünschte Größe
+        if (gradient == null)
+        {
+            Debug.LogError("Gradient has not been assigned in the HealthBar script.");
+            return; 
+        }
 
+        if (fill == null)
+        {
+            Debug.LogError("Fill Image has not been assigned in the HealthBar script.");
+            return;
+        }
+        
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        if (rectTransform == null)
+        {
+            Debug.LogError("RectTransform component not found on the HealthBar GameObject.");
+            return; 
+        }
+        
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.pivot = new Vector2(0, 0);
+        rectTransform.anchoredPosition = new Vector2(10, 10); 
+        rectTransform.sizeDelta = new Vector2(200, 30); 
     }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
-    {
-        slider.value = health;
-    }
 
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
 }
