7634a91174e76bcd541b85ff336c39f8503f3634
k0val94 <k0val94@users.noreply.github.com>
2023-11-19 17:57:47 +0100

    fix

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -52,7 +52,7 @@ public class HealthBar : MonoBehaviour
     }
 
     public void SetHealth(int health)
-
+	{
         slider.value = health;
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
