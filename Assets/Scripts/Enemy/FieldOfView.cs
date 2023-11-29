d14a785a60bdac2147cc4ac641b49e3da2b51131
k0val94 <k0val94@users.noreply.github.com>
2023-11-29 17:49:03 +0100

    FindVisibleTargets and OnDrawGizmos extended

--- Assets/Scripts/Enemy/FieldOfView.cs
+++ Assets/Scripts/Enemy/FieldOfView.cs
@@ -33,9 +33,14 @@ public class FieldOfView : MonoBehaviour
         foreach (Collider2D targetCollider in targetsInViewRadius)
         {
             Transform target = targetCollider.transform;
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+            float distanceToTarget = Vector2.Distance(transform.position, target.position);
 
-            visibleTargets.Add(target);
-            Debug.Log("Target " + target.name + " is visible.");
+            if (!Physics2D.Raycast(transform.position, dirToTarget, distanceToTarget, obstacleMask))
+            {
+                visibleTargets.Add(target);
+                Debug.Log("Target " + target.name + " is visible.");
+            }
         }
     }
 
@@ -43,5 +48,38 @@ public class FieldOfView : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, viewRadius);
+
+        if (visibleTargets != null)
+        {
+            foreach (Transform target in visibleTargets)
+            {
+                // Draw a line to each visible target
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, target.position);
+            }
+        }
+
+        // Draw raycasts
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+        foreach (Collider2D targetCollider in targetsInViewRadius)
+        {
+            Transform target = targetCollider.transform;
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+            float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToTarget, distanceToTarget, obstacleMask);
+            if (hit)
+            {
+                // Draw a red line if the target is blocked by an obstacle
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, hit.point);
+            }
+            else
+            {
+                // Draw a blue line if there's no obstacle blocking the view
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(transform.position, target.position);
+            }
+        }
     }
 }
